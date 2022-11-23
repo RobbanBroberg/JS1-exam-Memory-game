104cb292577a812a128e39621ec6db3742c994f7
j-carlander <johannes.carlander@gmail.com>
2022-11-23 14:32:21 +0100

    Initialized

---
+++ app.js
@@ -0,0 +1,33 @@
+// Memory examination exercise course JavScript 1, class KYHA_FE22
+
+// Variables including from DOM
+
+// object containing players name, score
+const players = [
+    { name: kalle, score: 0 }, // name from DOM via function start game
+    { name: pelle, score: 0 },
+];
+
+//Array ranomdizer insp. https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#Modern_method
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const result = [];
+let n = array.length;
+console.log(`n = ${n}`);
+for (i = n; i >= 1; i--) {
+    console.log(`run times left: ${i - 1}`);
+    let randomIndex = Math.floor(Math.random() * i);
+    console.log(randomIndex);
+    result.unshift(array[randomIndex]);
+    array.splice(randomIndex, 1);
+}
+console.log(result);
+
+// function start game => add players, hide registration, show game body, run game,
+
+// function Create cards data-value for compare
+
+// function Click card to flip
+
+// function compare cards
+
+// eventlistener
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Memory game
+
+## Rules
+
+1. Randomly placed cards, facing down
+2. Each player takes turns, choosing two cards.
+   a. If cards is matching pair, player get a score and to continue
+   b. If cards don't match, cards face down and next players turn
+3. The one with most points when all cards are opend, wins.
+
+two palyer game, sharing computer
+
+## if(have time)
+
+single player game, on time.
+background sound.
