b145d503437c7c572acc18f8ebac61aecee8917c
j-carlander <johannes.carlander@gmail.com>
2022-11-28 16:11:13 +0100

    fixing some functions

--- timetrialgame.js
+++ timetrialgame.js
@@ -1,3 +1,5 @@
+// Variables including from DOM
+
 const timeTrialScoreBoard = document.querySelector('.timetrial');
 
 const displayPlayerScore = document.querySelector('.player-score');
@@ -13,8 +15,6 @@ let minutes = 10,
 let minutesDisplayed = minutes < 10 ? `0${minutes}` : minutes;
 let secondsDisplayed = seconds < 10 ? `0${seconds}` : seconds;
 let timer = `${minutesDisplayed}:${secondsDisplayed}`;
-
-// function start game => add players, hide registration, show game body, run game,
 
 //function for a timer ticking down
 function timeLeft() {
@@ -32,28 +32,17 @@ function updateTimeTrialScoreBoard() {
     displayPlayerScore.innerText = playerScore;
 }
 
-function compareCardsTimeTrial(storedCards) {
-    let cardOneValue = storedCards[0].target.getAttribute('data-name');
-    let cardTwoValue = storedCards[1].target.getAttribute('data-name');
-
-    // let parent1 = storedCards[0].target.parentNode;
-    // let parent2 = storedCards[1].target.parentNode;
-
-    if (cardOneValue == cardTwoValue) {
-        //lock matching cards
-        removeListenerFromMatchingCards(storedCards);
-        //give points to the player::::
+function scoreTimeTrial(match) {
+    if (match == true) {
         playerScore += 1;
-    } else {
-        // Turn over non matching cards
-        returnNonMatchingCardsFaceDown(storedCards);
+        updateGameHistory(storedCards, 'Player');
     }
-    storedCards.splice(0, 2);
     updateTimeTrialScoreBoard();
 }
 
 function startTimeTrialGame() {
     cardsClickedCounter = 0;
+    playerScore = 0;
     let dubbleCards = cardArray.concat(cardArray);
     let randomizedCards = randomizeArray(dubbleCards);
     appendCardsToBoard(gameBoard, randomizedCards, 'single');
@@ -67,10 +56,12 @@ difficultyEasyBtn.addEventListener('click', () => {
     gameBodyContainer.setAttribute('style', 'display: flex;');
     timeTrialScoreBoard.setAttribute('style', 'display: block;');
     interval = setInterval(timeLeft, 1000);
+    gameMode = 'single';
     startTimeTrialGame();
 });
 
 restartGameBtn.addEventListener('click', () => {
     gameBoard.innerHTML = '';
+    gameHistoryList.innerHTML = '';
     startTimeTrialGame();
 });
