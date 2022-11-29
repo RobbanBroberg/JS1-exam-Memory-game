03a5134d5a218b04744292a3a512cbbd25c0f75b
j-carlander <johannes.carlander@gmail.com>
2022-11-29 09:53:52 +0100

    added more cards

--- app.js
+++ app.js
@@ -20,57 +20,6 @@ let cardPairsFound;
 
 const storedCards = []; // For storing which two cards is clicked for comparison
 
-const cardArray = [
-    {
-        title: '2morrow',
-        img: 'assets/2morrow.jpg',
-    },
-    {
-        title: 'aview',
-        img: 'assets/aview.jpg',
-    },
-    {
-        title: 'casino',
-        img: 'assets/casino.jpg',
-    },
-    {
-        title: 'dieanother',
-        img: 'assets/dieanother.jpg',
-    },
-    {
-        title: 'goldeneye',
-        img: 'assets/goldeneye.jpg',
-    },
-    {
-        title: 'licence',
-        img: 'assets/licence.jpg',
-    },
-    {
-        title: 'notime',
-        img: 'assets/notime.jpg',
-    },
-    {
-        title: 'quantum',
-        img: 'assets/quantum.jpg',
-    },
-    {
-        title: 'skyfall',
-        img: 'assets/skyfall.jpg',
-    },
-    {
-        title: 'spectre',
-        img: 'assets/spectre.jpg',
-    },
-    {
-        title: 'theliving',
-        img: 'assets/theliving.jpg',
-    },
-    {
-        title: 'theworld',
-        img: 'assets/theworld.jpg',
-    },
-];
-
 //Array ranomdizer insp. https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#Modern_method
 
 function randomizeArray(array) {
---
+++ cardarrays.js
@@ -0,0 +1,27 @@
+// list of all differnet cards.
+
+const cardArray = [
+    { title: '2morrow', img: 'assets/2morrow.jpg' },
+    { title: 'aview', img: 'assets/aview.jpg' },
+    { title: 'casino', img: 'assets/casino.jpg' },
+    { title: 'dieanother', img: 'assets/dieanother.jpg' },
+    { title: 'goldeneye', img: 'assets/goldeneye.jpg' },
+    { title: 'licence', img: 'assets/licence.jpg' },
+    { title: 'notime', img: 'assets/notime.jpg' },
+    { title: 'quantum', img: 'assets/quantum.jpg' },
+    { title: 'skyfall', img: 'assets/skyfall.jpg' },
+    { title: 'spectre', img: 'assets/spectre.jpg' },
+    { title: 'theliving', img: 'assets/theliving.jpg' },
+    { title: 'theworld', img: 'assets/theworld.jpg' },
+];
+
+// More cards for difficulty hard
+
+const moreCards = [
+    { title: 'diamonds', img: 'assets/diamonds.jpg' },
+    { title: 'foryour', img: 'assets/foryour.jpg' },
+    { title: 'goldfinger', img: 'assets/goldfinger.jpg' },
+    { title: 'moonraker', img: 'assets/moonraker.jpg' },
+    { title: 'octopussy', img: 'assets/octopussy.jpg' },
+    { title: 'thunderball', img: 'assets/thunderball.jpg' },
+];
